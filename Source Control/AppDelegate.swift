6d12e17168d48d1c15a8a8510a8012182c94befd
Jan Grodecki <jan.grodecki@rws.com>
2020-09-09 18:43:20 -0700

    Initial Commit

---
+++ Source Control/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Source Control
+//
+//  Created by Jan Grodecki on 9/9/20.
+//  Copyright © 2020 Jan Grodecki. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Source Control/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Source Control
+//
+//  Created by Jan Grodecki on 9/9/20.
+//  Copyright © 2020 Jan Grodecki. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
