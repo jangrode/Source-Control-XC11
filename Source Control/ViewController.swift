a9352ee2b4d9d9726c8ceee24efb20ca479d1a25
Jan Grodecki <jan.grodecki@rws.com>
2020-09-09 19:01:37 -0700

    Modified.

--- Source Control/ViewController.swift
+++ Source Control/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view. Modified.
     }
     
     /**
